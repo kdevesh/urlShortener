c7dd3b6b163d2b96ce6374eddd9c16112c649245
kdevesh <devesh.kumar@walmart.com>
2020-07-12 21:02:42 +0530

    Added Response Class

---
+++ src/main/java/com/devesh/urlShortener/response/Response.java
@@ -0,0 +1,13 @@
+package com.devesh.urlShortener.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response {
+    private int code;
+    private String message;
+}
