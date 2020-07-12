fd6ffa7ab31330e204cad9024da588cdb9a1a498
kdevesh <devesh.kumar@walmart.com>
2020-07-12 21:02:19 +0530

    Added @PrimaryKeyClass for User

--- src/main/java/com/devesh/urlShortener/models/User.java
+++ src/main/java/com/devesh/urlShortener/models/User.java
@@ -1,23 +1,21 @@
 package com.devesh.urlShortener.models;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.cassandra.core.cql.Ordering;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Table("user")
 public class User {
-    @PrimaryKeyColumn(name = "email", type = PrimaryKeyType.PARTITIONED)
-    private String email;
-    @PrimaryKeyColumn(name = "firstName", ordinal = 0, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
-    private String firstName;
-    @PrimaryKeyColumn(name = "lastName", ordinal = 1, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.ASCENDING)
-    private String lastName;
+    @PrimaryKey
+    private UserKey userKey;
+    @Column("createdon")
     private LocalDateTime createdOn;
 }
