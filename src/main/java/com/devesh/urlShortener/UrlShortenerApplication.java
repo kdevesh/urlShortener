7c7cca9d68b3116af499021682a5dd27ea8b2293
kdevesh <devesh.kumar@walmart.com>
2020-07-19 23:44:28 +0530

    Added BCryptPasswordEncode bean

--- src/main/java/com/devesh/urlShortener/UrlShortenerApplication.java
+++ src/main/java/com/devesh/urlShortener/UrlShortenerApplication.java
@@ -2,12 +2,23 @@ package com.devesh.urlShortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @SpringBootApplication
 public class UrlShortenerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(UrlShortenerApplication.class, args);
-	}
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 
 }
