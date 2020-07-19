569a3a472c62f0475d5f6049ca20df5ddd5262e4
kdevesh <devesh.kumar@walmart.com>
2020-07-19 23:45:24 +0530

    Added UserDetailService class

---
+++ src/main/java/com/devesh/urlShortener/service/UserDetailService.java
@@ -0,0 +1,34 @@
+package com.devesh.urlShortener.service;
+
+import com.devesh.urlShortener.models.User;
+import com.devesh.urlShortener.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        final User user = userRepository.findByUserKeyEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User '" + email + "' not found");
+        }
+
+        return org.springframework.security.core.userdetails.User//
+                .withUsername(email)//
+                .password(user.getPassword())//
+                .authorities("USER")//
+                .accountExpired(false)//
+                .accountLocked(false)//
+                .credentialsExpired(false)//
+                .disabled(false)//
+                .build();
+    }
+}
